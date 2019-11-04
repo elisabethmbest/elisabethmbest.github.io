1bfdf4169f363d74d94e8fbc7594025d159adae3
Elisabeth Best <elisabethmbest@gmail.com>
2019-11-03 19:19:01 -0700

    fixed recipe

---
+++ recipes/precache-manifest.6f997d85a607d3c1a1b5745b51d4c0d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb4c464d476e86e4c78e7addeb2ee654",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2911c9199f67f72e21c",
+    "url": "/static/css/main.a222bdc7.chunk.css"
+  },
+  {
+    "revision": "7dedf20ddaa54a2fc857",
+    "url": "/static/js/2.f0440a27.chunk.js"
+  },
+  {
+    "revision": "b2911c9199f67f72e21c",
+    "url": "/static/js/main.f4da5984.chunk.js"
+  },
+  {
+    "revision": "d6fa8869a97c1a04a5c8",
+    "url": "/static/js/runtime-main.cf7ea07e.js"
+  }
+]);
